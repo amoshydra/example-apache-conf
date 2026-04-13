1f653fef6f54adedbb8088ddcd1caf65dc6e941c
amoshydra <8733840+amoshydra@users.noreply.github.com>
2026-04-14 00:16:59 +0800

    fix: fix

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ CONTAINER_NAME="apache-learning"
 podman run -d \
   --name "$CONTAINER_NAME" \
   -p 8080:80 \
-  -v "$(pwd)/conf:/usr/local/apache2/conf:ro" \
-  -v "$(pwd)/htdocs:/usr/local/apache2/htdocs:ro" \
+  -v "$(pwd)/htdocs:/usr/local/apache2/htdocs" \
   "$IMAGE_NAME"
 
 echo "Apache running at http://localhost:8080"
